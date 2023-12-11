8e5bf8613bbc8be8ede9be7e3bdf1cc774ffcfea
Orapeleng Mathebula <71043883+o897@users.noreply.github.com>
2023-12-11 22:25:17 +0200

    Update layout.blade.php added fontawesome js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,7 @@
     <title>Admin</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('fontawesome-free-6.4.0-web/css/all.min.css') }}">
+    <script src="{{asset('fontawesome-free-6.4.0-web/js/all.min.js')}}"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
     {{-- <link rel="stylesheet" href="{{ asset('bootstrap-5.0.2/dist/css/bootstrap.min.css') }}"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
